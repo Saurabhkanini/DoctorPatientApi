efcacfccc36f214cc4b4d3fd0890762d670c32ee
saurabhkanini <saurabh.kanini@gmail.com>
2023-07-03 02:07:43 +0530

    uploading api for BigBang Assessment2

---
+++ DoctorPatientsManagementApi/Controllers/AuthController.cs
@@ -0,0 +1,78 @@
+﻿using DoctorPatientsManagementApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace DoctorPatientsManagementApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly DoctorsDbContext mdbc;
+        private readonly IConfiguration configuration;
+
+        public AuthController(DoctorsDbContext m, IConfiguration ic)
+        {
+            this.mdbc = m;
+            this.configuration = ic;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Index()
+        {
+            var user = await mdbc.registerUsers.ToListAsync();
+            return Ok(user);
+        }
+        [HttpPost("register")]
+        public async Task<ActionResult> AddUser(RegisterUser r)
+        {
+            var users = mdbc.registerUsers.FirstOrDefault(x => x.UserName == r.UserName);
+            if (users == null)
+            {
+                mdbc.registerUsers.Add(r);
+                mdbc.SaveChanges();
+                return Ok(r);
+            }
+            return BadRequest(new { error = "UserName Already Exists" });
+
+        }
+        [HttpPost("login")]
+        public async Task<ActionResult> LoginUser(LoginUser l)
+        {
+            var user = await mdbc.registerUsers.FirstOrDefaultAsync(x => x.UserName == l.UserName);
+            if (user != null)
+            {
+                if (user.Password == l.Password)
+                {
+                    var token = CreatToken(user);
+                    return Ok(new { token, user });
+                }
+            }
+            return NotFound("User Not Found With Provided Detail");
+
+        }
+        private string CreatToken(RegisterUser ud)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, ud.UserName),
+                new Claim(ClaimTypes.Role,ud.Role)
+            };
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration.GetSection("AppSetting:Token").Value!));
+            var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: cred
+                );
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+            return jwt;
+        }
+    }
+}
---
+++ DoctorPatientsManagementApi/Controllers/PatientController.cs
@@ -0,0 +1,77 @@
+﻿using DoctorPatientsManagementApi.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DoctorPatientsManagementApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PatientController : ControllerBase
+    {
+        private readonly DoctorsDbContext mdbc;
+        public PatientController(DoctorsDbContext tdbc)
+        {
+            this.mdbc = tdbc;
+        }
+        [HttpGet]
+        [Authorize(Roles = "admin")]
+        [AllowAnonymous]
+        public async Task<ActionResult> GetAllPatients()
+        {
+            if (!User.Identity.IsAuthenticated || !User.IsInRole("admin"))
+            {
+                return Unauthorized("You are Not authorized");
+            }
+            var patients = await mdbc.Patients.ToListAsync();
+            return Ok(patients);
+        }
+        [HttpPost]
+        public async Task<ActionResult> AddPatient(Patient t)
+        {
+            await mdbc.Patients.AddAsync(t);
+            await mdbc.SaveChangesAsync();
+            return Ok(t);
+        }
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdatePatient(Patient t, int id)
+        {
+            var patient = await mdbc.Patients.FindAsync(id);
+            if (patient == null)
+            {
+                return BadRequest($"Patient Not find With id = {id}");
+            }
+            patient.pname = t.pname;
+            patient.plocation = t.plocation;
+            patient.pissue = t.pissue;
+           
+            mdbc.Patients.Update(patient);
+            mdbc.SaveChanges();
+            return Ok(patient);
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeletePatient(int id)
+        {
+            var patient = await mdbc.Patients.FindAsync(id);
+            if (patient == null)
+            {
+                return NotFound($"Patient Not Found with id = {id}");
+            }
+            mdbc.Patients.Remove(patient);
+            await mdbc.SaveChangesAsync();
+            return Ok(patient);
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetPatient(int id)
+        {
+            var patient = await mdbc.Patients.FindAsync(id);
+            if (patient == null)
+            {
+                return NotFound($"Patient Not Found with id = {id}");
+            }
+            return Ok(patient);
+
+        }
+    }
+}
